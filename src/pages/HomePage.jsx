5e9195a4ec6f67a5a706107e334f3ac727b85b5b
Goodomenn <dev@elatradingplc.com>
2026-09-21 01:45:30 +0300

    Remove HOW TO TRADE section banner from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import HowToTrade from '../components/HowToTrade';
 import DivisionFilter from '../components/DivisionFilter';
 import FeaturedSection from '../components/FeaturedSection';
 import CateringIntroSection from '../components/CateringIntroSection';
@@ -8,10 +7,7 @@ import BricksIntroSection from '../components/BricksIntroSection';
 export default function HomePage({ activeFilter, onSelectFilter }) {
   return (
     <>
-      {/* 3-Step Process Ribbon */}
-      <HowToTrade />
-
-      {/* Page Title & Division Filter Pills (No subcategory ticker as requested) */}
+      {/* Page Title & Division Filter Pills */}
       <DivisionFilter 
         activeFilter={activeFilter} 
         onSelectFilter={onSelectFilter} 
@@ -20,10 +16,10 @@ export default function HomePage({ activeFilter, onSelectFilter }) {
       {/* 3 Arched Capsule Cards (Bricks, Catering, Coming Soon with Hover Visit Animation) */}
       <FeaturedSection />
 
-      {/* Introduction to Ela Catering Services (holding the signature structure) */}
+      {/* Introduction to Ela Catering Services */}
       <CateringIntroSection />
 
-      {/* Below Catering: Introduction to Bricks Lounge (holding the signature structure) */}
+      {/* Introduction to Bricks Lounge */}
       <BricksIntroSection />
     </>
   );
